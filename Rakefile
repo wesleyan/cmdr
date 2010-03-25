19837082db75605404178ead4e1120f3cb26e7ba
mwylde <mwylde@wesleyan.edu>
2010-03-25 15:59:29 -0400

    Five-way controls now work; fixed projector images

--- Rakefile
+++ Rakefile
@@ -3,7 +3,7 @@ require 'extlib'
 WORKING = File.dirname(__FILE__)
 TARGETS = ['/tp5']
 
-SERVERS = ["129.133.41.247"]
+SERVERS = ["129.133.124.142"]
 
 OPTS = {}
 
--- wescontrol_web/apps/tp5/modules/dvd/DVD_module.js
+++ wescontrol_web/apps/tp5/modules/dvd/DVD_module.js
@@ -21,6 +21,53 @@ Tp5.DVDModule = SC.View.extend(Tp5.MouseHandlingFix,
 		'play', 'back', 'pause', 'forward', 'stop', 'previous', 'next', 'menu', 'title'
 	],
 	
+	regions: [
+		{
+			action: 'enter',
+			shape: "circle",
+			center: [77, 77],
+			radius: 23
+		},
+		{
+			action: "left",
+			coords: [[77,77], [23, 23], [0,77], [23, 131]]
+		},
+		{
+			action: "right",
+			coords: [[77,77], [131,23], [170,75], [131,131]]
+		},
+		{
+			action: "up",
+			coords: [[77,77], [131, 23], [77,0], [23,23]]
+		},
+		{
+			action: "down",
+			coords: [[77,77], [131, 131], [77, 170], [23, 131]]
+		}
+	],
+	
+	pointInPolygon: function(poly, point){
+		var x = point[0];
+		var y = point[1];
+		if(poly.shape == "circle")
+		{
+			return Math.pow(x-poly.center[0],2) + Math.pow(y - poly.center[1],2) < Math.pow(poly.radius,2);
+		}
+		else
+		{
+			var c, i, l, j;
+			poly = poly.coords;
+		    for(c = false, i = -1, l = poly.length, j = l - 1; ++i < l; j = i)
+			{
+				var test1 = (poly[i][1] <= y && y < poly[j][1]) || (poly[j][1] <= y && y < poly[i][1]);
+				var test2 = x < (poly[j][0] - poly[i][0]) * (y - poly[i][1]) / (poly[j][1] - poly[i][1]) + poly[i][0];
+		        if(test1 && test2)c = !c;
+			}
+		    return c;
+		}
+	},
+	
+	
 	render: function(context, firstTime) {
 		var content = this.get('content');
 		
@@ -40,29 +87,45 @@ Tp5.DVDModule = SC.View.extend(Tp5.MouseHandlingFix,
 		
 		context = context.end();
 		
-		context = context.begin('div').addClass('five-way')
-			.begin('div').addClass('right fbutton').end()
-			.begin('div').addClass('down fbutton').end()
-			.begin('div').addClass('left fbutton').end()
-			.begin('div').addClass('up fbutton').end()
-			.begin('div').addClass('center fbutton').end()
-		.end();
+		context = context.begin('div').addClass('five-way').end();
+		context = context.end().end().end;
 		
 		sc_super();
 	},
 	
-	mouseClicked: function(evt){
-		if(evt.target.title && evt.target.className.split(" ").some(function(x){ return x == "dvd-button"; }))
+	mouseDown: function(evt){
+		if(evt.target.className.split(" ").some(function(x){return x == "five-way";}))
 		{
-			if(Tp5.roomController.get('dvdplayer'))
+			//unfortunately, we have to use js to inspect the coordinates of the touch in order to see
+			//which button was pressed. I wish there was a better way to do this, but there appears not
+			//to be.			
+			var x = evt.originalEvent.offsetX;
+			var y = evt.originalEvent.offsetY;
+			for(var i = 0; i < this.regions.get('length'); i++)
 			{
-				Tp5.roomController.get('dvdplayer').send_command("pulse_command", evt.target.title);
+				if(this.pointInPolygon(this.regions.objectAt(i), [x,y]))
+				{
+					SC.$('.five-way')[0].className = "five-way " + this.regions.objectAt(i).action;
+					break;
+				}
 			}
 		}
-		else if(evt.target.className.split(" ").some(function(x){return x == "fbutton";}))
+	},
+	
+	mouseClicked: function(evt){
+		var action = null;
+		if(evt.target.title && evt.target.className.split(" ").some(function(x){ return x == "dvd-button"; }))
 		{
-			console.log(evt);
-			//console.log(evt.target.className.split(" ").find(function(x){return x != "fbutton";}));
+			action = evt.target.title;
+		}
+		else if(SC.$('.five-way')[0].className.split(" ").some(function(x){ return x != "five-way";}))
+		{
+			action = SC.$('.five-way')[0].className.split(" ").find(function(x){ return x != "five-way";});
+			SC.$('.five-way')[0].className = "five-way";
+		}
+		if(Tp5.roomController.get('dvdplayer') && action)
+		{
+			Tp5.roomController.get('dvdplayer').send_command("pulse_command", evt.target.title);
 		}
 	}
 
