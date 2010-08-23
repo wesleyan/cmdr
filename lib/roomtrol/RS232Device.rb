0c894aaf7e5a9a7473d7f2a5fa42ccedd2c8f6c4
Micah Wylde <mwylde@wesleyan.edu>
2010-08-23 00:28:59 -0700

    More device docs Updated specs to work with 1.9.2 (fixed issues caused by removing '.' from the load path)

--- lib/roomtrol/RS232Device.rb
+++ lib/roomtrol/RS232Device.rb
@@ -9,7 +9,7 @@ module Wescontrol
 	# will handle communication through a serial port (using the
 	# [ruby-serialport](http://ruby-serialport.rubyforge.org/) library) for you, providing
 	# {#send_string} and {#read}, which respectively send and receive 
-	# strings to and from the device. However, most devices should be able to take advatange
+	# strings to and from the device. However, most devices should be able to take advantage
 	# of the various "managed" features, which can greatly simplify the task of writing drivers
 	# for RS232 devices. In fact, using this class it is possible to write drivers without any
 	# real code at all besides some string handling.
@@ -48,7 +48,6 @@ module Wescontrol
 	# 		:type => :option, 
 	# 		:display_order => 1, 
 	# 		:options => ("1".."6").to_a,
-	# 		:response => :channel,
 	# 		:action => proc{|input|
 	# 			"#{input}!\r\n"
 	# 		}
@@ -69,6 +68,18 @@ module Wescontrol
 	# 		send :volume, "V\r\n", 0.5
 	# 		send :mute, "Z\r\n", 0.5
 	# 	end
+	# 
+	# These abstractions are designed around devices that have the following message lifecycle:
+	# 
+	# 1. A message is sent from the controller to the device
+	# 2. The device processes the event and sends back a response, either an acknowledge or a full response.
+	# 3. The device is now ready for a new message, starting the cycle over.
+	# 
+	# In particular, since many devices can only handle one message at a time, RS232Device waits
+	# until a response is received or {RS232Device::TIMEOUT TIMEOUT} seconds have passed before
+	# sending the next message in the queue. Devices with more complex message handling (for example,
+	# those with multiple message queues) may not work optimally with this strategy. In those cases,
+	# writing the message handling system yourself may be preferable. 
 	class RS232Device < Device
 		# The number of seconds to way for a reply before transmitting the next message
 		TIMEOUT = 0.5
@@ -83,11 +94,39 @@ module Wescontrol
 			parity 0
 			message_end "\r\n"
 		end
+		
+		# Creates a new RS232Device instance
+		# @param [String, Symbol] name The name of the device, which is stored in the database
+		# 	and used to communicate with it over AMQP
+		# @param [Hash{String, Symbol => Object}] hash A hash of configuration definitions, from
+		# 	the name of the config var to its value
+		# @param [String] db_uri The URI of the CouchDB database where updates should be saved
+		# @param [String] dqueue The AMQP queue that the device watches for messages
+		def initialize(name, options, db_uri = "http://localhost:5984/rooms", dqueue = nil)
+			options = options.symbolize_keys
+			@port = options[:port]
+			throw "Must supply serial port parameter" unless @port
+			@baud = options[:baud] ? options[:baud] : 9600
+			@data_bits = options[:data_bits] ? options[:data_bits] : 8
+			@stop_bits = options[:stop_bits] ? options[:stop_bits] : 1
+			@parity = options[:parity] ? options[:parity] : 0
+			@connection = RS232Connection.dup
+			@connection.instance_variable_set(:@receiver, self)
+			
+			@_send_queue = []
+			@_ready_to_send = true
+			super(name, options, db_uri, dqueue)
+		end
 
+		# Sends a string to the serial device
+		# @param [String] string The string to send
 		def send_string(string)
 			@serialport.send_data(string) if @serialport
 		end
 		
+		# Run is a blocking call that starts the device. While run is running, the device will
+		# watch for AMQP events as well as whatever communication channels the device uses and
+		# react appropriately to events. It will also send requests if any have been specified.
 		def run
 			EM::run {
 				begin
@@ -101,40 +140,61 @@ module Wescontrol
 			}
 			
 		end
-	
-		def initialize(name, options)
-			options = options.symbolize_keys
-			@port = options[:port]
-			throw "Must supply serial port parameter" unless @port
-			@baud = options[:baud] ? options[:baud] : 9600
-			@data_bits = options[:data_bits] ? options[:data_bits] : 8
-			@stop_bits = options[:stop_bits] ? options[:stop_bits] : 1
-			@parity = options[:parity] ? options[:parity] : 0
-			@connection = RS232Connection.dup
-			@connection.instance_variable_set(:@receiver, self)
-			
-			@_send_queue = []
-			@_ready_to_send = true
-			super(name, options)
-		end
 		
+		# This method, when called in a class body, creates a managed state var. A managed state
+		# var is very much like a normal {Device::state_var state_var}, but expects that a proc
+		# be supplied to action which returns the string to send to the device. It then creates 
+		# the set_* method automatically and handles all device communication for you.
+		# @param [Symbol] name The name for the managed state var. Should follow the general
+		# 	naming conventions: all lowercase, with multiple words connected\_by\_underscores.
+		# @param [Hash] options Options for configuring the managed\_state\_var
+		# @option options [Symbol] :type [mandatory] the type of the variable; this is
+		# 	used by the web interface to decide what kind of interface to show. Possible
+		# 	values are :boolean, :string, :percentage, :number, :decimal, :option, and
+		# 	:array.
+		# @option options [Boolean] :editable (true) whether or not this variable can be set
+		# 	by the user.
+		# @option options [Integer] :display_order Used by the web interface to decide which
+		# 	variables are visible and in which order they are displayed. For a particular
+		# 	device, the var with the lowest :display_order is ranked highest, followed by
+		# 	the next lowest up to 6. Leave out if the variable should not be shown.
+		# @option options [Array<#to_json>] :options If :option is selected for type, the
+		# 	elements in this array serve as the allowable options.
+		# @option options [Proc] :action [mandatory] If a proc is supplied to :action, then
+		# 	managed\_state\_varwill automatically create a set_varname method (where varname
+		# 	is the name of the state variable) which executes the code provided. This proc 
+		# 	should return the string which, when sent to the device, will cause it to enter
+		# 	the chosen state.
+		# @example
+		# 	maanged_state_var :input, 
+		# 		:type => :option, 
+		# 		:display_order => 1, 
+		# 		:options => ("1".."6").to_a,
+		# 		:action => proc{|input|
+		# 			"{input}!\r\n"
+		# 		}
 		def self.managed_state_var name, options
+			throw "Must supply a proc to :action" unless options[:action].is_a? Proc
 			state_var name, options
-			if options[:action].is_a? Proc
-				define_method "set_#{name}".to_sym, proc {|value|
-					message = options[:action].call(value)
-					deferrable = EM::DefaultDeferrable.new
-					do_message message, deferrable
-					deferrable
-				}
-			end
+			define_method "set_#{name}".to_sym, proc {|value|
+				message = options[:action].call(value)
+				deferrable = EM::DefaultDeferrable.new
+				do_message message, deferrable
+				deferrable
+			}				
 		end
 		
+		# @private
+		# Adds a message to the back of the send queue, creating it if it does not exist
+		# @param [String] message The message to add
+		# @param [EM::Deferrable] deferrable The deferrable associated with the message
 		def do_message(message, deferrable = nil)
 			@_send_queue ||= []
 			@_send_queue.unshift [message, deferrable]
 		end
 		
+		# @private
+		# A simple class which is used for parsing responses by means of instance_eval
 		class ResponseHandler
 			attr_reader :matchers
 			def initialize
@@ -154,6 +214,43 @@ module Wescontrol
 			end
 		end
 		
+		# Starts a responses block, wherein statements which describe the various kinds of
+		# responses the device can send are placed. There are four kinds of responses:
+		# 
+		# + *match*, which is used for general responses
+		# + *ack*, which is used for acknowledge responses, sent by devices to announce
+		# 	that the message has been received and the next can be sent
+		# + *nack*, not acknowledged, meaning that the message was received but could not
+		# 	be interpretted or executed
+		# + *error*, meaning that there was some error running the previous message
+		# 
+		# Match is used most frequently, whereas the others are applicable only to some devices
+		# which have those features. Each response takes a matcher, which can either be a string,
+		# regular expression or a proc. Whenever a message is received, it is compared to each
+		# of the matchers that have been defined in order, and the first matcher to match (defined
+		# as being equal for strings, returning a non-nil value from `String#match` for regular
+		# expression, or returning a true value when supplied the message for procs) is used.
+		# When a response created with `match` is selected, the supplied "interpreter" proc is called,
+		# which will generally update state vars. The argument to the interpreter depends on the
+		# matcher type. For strings, no argument is supplied; for regular expressions, the matcher
+		# object is supplied; for procs, the original message is supplied. When an `error` response is 
+		# matched, the supplied message is returned to the user. When either an `ack` or `nack` response 
+		# is matched, the next message is sent and no futher action taken.
+		# 
+		# `match` takes three arguments: a name, a matcher and an interpreter. `nack` and `ack` each
+		# take only a matcher. `error` takes a name, a matcher and a string message.
+		# @example
+		# 	responses do
+		# 		#regular expression matcher
+		# 		match :volume, /Vol(\d+)/, proc{|m| self.volume = m[1].to_i/100.0}
+		# 		#string matcher
+		# 		match :powered_on, "power=on", proc{ self.power = true}
+		# 		#proc matcher
+		# 		match :input, proc{|m| m[0] == 2 && m[1] == 0x15}, proc{|m| self.input = m[4].to_i * 2**8 + m[5].to_i}
+		# 		ack "ack"
+		# 		nack "nack"
+		# 		error :power_error, "power_error", "Projector failed to turn on"
+		# 	end
 		def self.responses &block
 			rh = ResponseHandler.new
 			rh.instance_eval(&block)
@@ -165,6 +262,8 @@ module Wescontrol
 			self.class.instance_variable_get(:@_matchers)
 		end
 		
+		# @private
+		# A simple class which is used for parsing requests by means of instance_eval
 		class RequestHandler
 			attr_reader :requests
 			def send name, req, freq
--- spec/managedrs232device_spec.rb
+++ spec/managedrs232device_spec.rb
@@ -1,14 +1,10 @@
-require File.dirname(__FILE__) + '/spec_helper.rb'
-require File.dirname(__FILE__) + '/../lib/roomtrol/device.rb'
-require File.dirname(__FILE__) + '/../lib/roomtrol/rs232device.rb'
-#require File.dirname(__FILE__) + '/../lib/roomtrol/managedrs232device.rb'
-
-# Time to add your specs!
-# http://rspec.info/
+require_relative 'spec_helper.rb'
+require_relative '../lib/roomtrol/device.rb'
+require_relative '../lib/roomtrol/rs232device.rb'
 
 describe "managed_state_var enhancements" do	
 	it "shouldn't break managed_state_vars" do
-		class MR232DeviceSubclass < Wescontrol::RS232Device
+		class MR232DeviceSubclass1 < Wescontrol::RS232Device
 			managed_state_var :input, 
 				:type => :options, 
 				:display_order => 1, 
@@ -18,10 +14,10 @@ describe "managed_state_var enhancements" do
 					"#{input}!\r\n"
 				}
 		end
-		MR232DeviceSubclass.state_vars[:input][:type] == :option
+		MR232DeviceSubclass1.state_vars[:input][:type] == :option
 	end
 	it "should create action methods that add the message to the send queue" do
-		class MR232DeviceSubclass < Wescontrol::RS232Device
+		class MR232DeviceSubclass2 < Wescontrol::RS232Device
 			attr_accessor :_send_queue
 			managed_state_var :input, 
 				:type => :options, 
@@ -32,7 +28,7 @@ describe "managed_state_var enhancements" do
 					"#{input}!\r\n"
 				}
 		end
-		ds = MR232DeviceSubclass.new("Extron", :port => "/dev/null")
+		ds = MR232DeviceSubclass2.new("Extron", :port => "/dev/null")
 		ds.set_input(4)
 		ds._send_queue[0][0].should == "4!\r\n"
 	end
@@ -40,7 +36,7 @@ end
 
 describe "do responses" do
 	it "should respond to responses" do
-		class MR232DeviceSubclass < Wescontrol::RS232Device
+		class MR232DeviceSubclass3 < Wescontrol::RS232Device
 			responses do
 			end
 		end
@@ -48,28 +44,28 @@ describe "do responses" do
 
 	it "should allow setting responses with match" do
 		$proc = proc{|m| self.input = m[1]}
-		class MR232DeviceSubclass < Wescontrol::RS232Device
+		class MR232DeviceSubclass4 < Wescontrol::RS232Device
 			responses do
 				match :channel,  /Chn(\d)/, $proc
 			end
 		end
-		MR232DeviceSubclass.instance_variable_get(:@_matchers)[0].should == [:channel, /Chn(\d)/, $proc]
+		MR232DeviceSubclass4.instance_variable_get(:@_matchers)[0].should == [:channel, /Chn(\d)/, $proc]
 	end
 	it "should allow setting multiple matches" do
 		$proc = proc{|m| self.input = m[1]}
-		class MR232DeviceSubclass < Wescontrol::RS232Device
+		class MR232DeviceSubclass5 < Wescontrol::RS232Device
 			responses do
 				match :channel,  /Chn(\d)/, $proc
 				match :volume, /Vol(\d)/, $proc
 			end
 		end
-		MR232DeviceSubclass.instance_variable_get(:@_matchers).should == [
+		MR232DeviceSubclass5.instance_variable_get(:@_matchers).should == [
 			[:channel, /Chn(\d)/, $proc],
 			[:volume, /Vol(\d)/, $proc],
 		]
 	end
-	it "should properly match regexps" do
-		class MR232DeviceSubclass < Wescontrol::RS232Device
+	it "should properly match regexps, strings and procs" do
+		class MR232DeviceSubclass6 < Wescontrol::RS232Device
 			managed_state_var :input, 
 				:type => 'option', 
 				:display_order => 1, 
@@ -88,19 +84,25 @@ describe "do responses" do
 			responses do
 				match :channel,  /Chn(\d)/, proc{|m| self.input = m[1].to_i}
 				match :volume,   /Vol(\d+)/, proc{|m| self.volume = m[1].to_i/100.0}
+				match :something, "hello", proc{ self.volume = 22 }
+				match :else, proc {|x| x.to_i != 0}, proc{|m| self.volume = m.to_i}
 			end
 		end
-		ds = MR232DeviceSubclass.new("Extron", :port => '/dev/null')
+		ds = MR232DeviceSubclass6.new("Extron", :port => '/dev/null')
 		ds.read "Chn4\r\n"
 		ds.read "Vol12\r\n"
 		ds.input.should == 4
 		ds.volume.should == 0.12
+		ds.read "hello\r\n"
+		ds.volume.should == 22
+		ds.read "512\r\n"
+		ds.volume.should == 512
 	end
 end
 
 describe "do requests" do
 	it "should properly send requests" do
-		class MR232DeviceSubclass < Wescontrol::RS232Device
+		class MR232DeviceSubclass7 < Wescontrol::RS232Device
 			attr_reader :string_array
 			def initialize(name, options)
 				super(name, options)
@@ -121,7 +123,7 @@ describe "do requests" do
 				@string_array << string
 			end
 		end
-		ds = MR232DeviceSubclass.new("Extron", :port => "/dev/null")
+		ds = MR232DeviceSubclass7.new("Extron", :port => "/dev/null")
 		EM::run {
 			EM::add_periodic_timer(3) {
 				AMQP.stop do
@@ -136,7 +138,7 @@ end
 
 describe "sending messages" do
 	it "should respond to AMQP messages appropriately" do
-		class MR232DeviceSubclass < Wescontrol::RS232Device
+		class MR232DeviceSubclass8 < Wescontrol::RS232Device
 			attr_reader :string_array
 			def initialize(name, options)
 				super(name, options)
@@ -151,7 +153,7 @@ describe "sending messages" do
 			end
 		end
 		
-		ds = MR232DeviceSubclass.new('Extron', :port => '/dev/null')
+		ds = MR232DeviceSubclass8.new('Extron', :port => '/dev/null')
 		json = '{
 			"id": "FF00F317-108C-41BD-90CB-388F4419B9A1",
 			"queue": "roomtrol:test:3",
