e87462d04cc7e8cc733b0d9fe3b7d4666964bc00
mwylde <mwylde@wesleyan.edu>
2010-11-29 12:10:13 -0500

    Changes to BenQ

--- BenQSP890Projector.rb
+++ BenQSP890Projector.rb
@@ -10,15 +10,15 @@
 
 class BenQSP890Projector < Projector
 	configure do
-		baud           115200
-		message_format(/\r\*(.*)#\r/)
+		baud           9600
+		message_format(/\*(.*)#\r/)
 	end
 	
 	managed_state_var :power, 
 		:type => :boolean,
 		:display_order => 1,
 		:action => proc{|on|
-			"\r*pow=#{on ? "on" : "off"}#\r"
+			"*pow=#{on ? "on" : "off"}#\r"
 		}
 	
 	managed_state_var :input, 
@@ -26,20 +26,20 @@ class BenQSP890Projector < Projector
 		:options => ['VGA', 'DVI', 'COMP', 'SVID'],
 		:display_order => 2,
 		:action => proc{|source|
-			"\r*sour=#{source}#\r"
+			"*sour=#{source}#\r"
 		}
 
 	managed_state_var :mute, 
 		:type => :boolean,
 		:action => proc{|on|
-			"\r*mute=#{on ? "on" : "off"}#\r"
+			"*mute=#{on ? "on" : "off"}#\r"
 		}
 	
 	managed_state_var :video_mute,
 		:type => :boolean,
 		:display_order => 4,
 		:action => proc{|on|
-			"\r*blank=#{on ? "on" : "off"}#\r"
+			"*blank=#{on ? "on" : "off"}#\r"
 		}
 		
 	responses do
@@ -47,7 +47,7 @@ class BenQSP890Projector < Projector
 			self.power = (m[1] == "on")
 			self.cooling = (m[1] == "cool down")
 		}
-		match :mute,       /mute=(.+)/, proc{|m| self.mute = (m[1] == "on"})
+		match :mute,       /mute=(.+)/, proc{|m| self.mute = (m[1] == "on")}
 		match :video_mute, /blank=(.+)/, proc{|m| self.video_mute = (m[1] == "on")}
 		match :input,      /sour=(.+)/, proc{|m| self.input = m[1]}
 	end
