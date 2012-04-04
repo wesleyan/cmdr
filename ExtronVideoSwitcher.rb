e4ec0286ce3873c6a455699c4ad32fe4e8c55588
Brian Gapinski <bgapinski@gmail.com>
2012-04-04 15:46:58 -0400

    Begin adding sound breakaway to extron

--- ExtronVideoSwitcher.rb
+++ ExtronVideoSwitcher.rb
@@ -14,24 +14,40 @@ class ExtronVideoSwitcher < VideoSwitcher
 		message_end "\r\n"
 	end
 	
-	managed_state_var :input, 
-		:type => :option, 
-		:display_order => 1, 
-		:options => ("1".."6").to_a,
-		:response => :channel,
-		:action => proc{|input|
-			"#{input}!"
-		}
+	#managed_state_var :input, 
+	#	:type => :option, 
+	#	:display_order => 1, 
+	#	:options => ("1".."6").to_a,
+	#	:response => :channel,
+	#	:action => proc{|input|
+	#		"#{input}!"
+	#	}
+  managed_state_var :video,
+    :type => :option,
+    :display_order => 1,
+    :options => ("1".."6").to_a,
+    :response => :channel,
+    :action => proc{|input|
+      "#{input}&"
+    }
+  managed_state_var :audio,
+    :type => :option,
+    :display_order => 2,
+    :options => ("1".."6").to_a,
+    :response => :channel,
+    :action => proc{|input|
+      "#{input}$"
+    }
 	managed_state_var :volume,
 		:type => :percentage,
-		:display_order => 2,
+		:display_order => 3,
 		:response => :volume,
 		:action => proc{|volume|
 			"#{(volume*100).to_i}V"
 		}
 	managed_state_var :mute,
 		:type => :boolean,
-		:display_order => 3,
+		:display_order => 4,
 		:response => :mute,
 		:action => proc{|on|
 			on ? "1Z" : "0Z"
@@ -40,15 +56,17 @@ class ExtronVideoSwitcher < VideoSwitcher
 	state_var :model, :type => 'string', :editable => false
 	state_var :firmware_version, :type => 'string', :editable => false
 	state_var :part_number, :type => 'string', :editable => false
-	state_var :clipping, :type => 'boolean', :display_order => 4, :editable => false
+	state_var :clipping, :type => 'boolean', :display_order => 5, :editable => false
 	
 	responses do
 		match :channel,  /Chn(\d)/, proc{|m| self.input = m[1].to_i.to_s}
 		match :volume,   /Vol(\d+)/, proc{|m| self.volume = m[1].to_i/100.0}
 		match :mute,     /Amt(\d+)/, proc{|m| self.mute = m[1] == "1"}
 		match :status,   /Vid(\d+) Aud(\d+) Clp(\d)/, proc{|m|
-			self.input = m[1].to_i if m[1].to_i > 0
-			self.clipping = (m[2] == "1")
+			#self.input = m[1].to_i if m[1].to_i > 0
+      self.video = m[1].to_i if m[1].to_i > 0
+      self.audio = m[2].to_i if m[2].to_i > 0
+			self.clipping = (m[3] == "1")
 		}
     match :input, /Mod(\d+) (\d)G(\d) (\d)G(\d) (\d)G(\d) (\d)G(\d)=(\d)G(\d)/, proc{|m|
       x1, x2 = [m[10].to_i, m[11].to_i]
@@ -63,7 +81,9 @@ class ExtronVideoSwitcher < VideoSwitcher
 	end
 	
 	requests do
-		send :input, "I", 0.5
+		#send :input, "I", 0.5
+    send :video, "I", 0.5
+    send :audio, "I", 0.5
 		send :volume, "V", 0.5
 		send :mute, "Z", 0.5
 	end
