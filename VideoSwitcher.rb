b5ce44f6f3d87ffc829d3111e3cae9635bdf8137
Brian Gapinski <bgapinski@gmail.com>
2012-04-06 15:55:24 -0400

    Breakaway audio support added to ExtronVideoSwitcher

--- VideoSwitcher.rb
+++ VideoSwitcher.rb
@@ -11,7 +11,9 @@
 #---
 
 class VideoSwitcher < Wescontrol::RS232Device
-	state_var :input, :type => :option, :display_order => 1, :options => ("1".."6").to_a
-	state_var :volume, :type => :percentage, :display_order => 2
-	state_var :mute, :type => :boolean, :display_order => 3
+	#state_var :input, :type => :option, :display_order => 1, :options => ("1".."6").to_a
+  state_var :video, :type => :option, :display_order => 1, :options => ("1".."6").to_a
+  state_var :audio, :type => :option, :display_order => 2, :options => ("1".."6").to_a
+	state_var :volume, :type => :percentage, :display_order => 3
+	state_var :mute, :type => :boolean, :display_order => 4
 end
