64d9672b923d56747a038eb25d04639915baf12a
Brian Gapinski <bgapinski@gmail.com>
2012-04-23 16:31:26 -0400

    Some work from another branch slipped in. Correcting.

--- ExtronVideoSwitcher.rb
+++ ExtronVideoSwitcher.rb
@@ -14,14 +14,14 @@ class ExtronVideoSwitcher < VideoSwitcher
 		message_end "\r\n"
 	end
 	
-	#managed_state_var :input, 
-	#	:type => :option, 
-	#	:display_order => 1, 
-	#	:options => ("1".."6").to_a,
-	#	:response => :channel,
-	#	:action => proc{|input|
-	#		"#{input}!"
-	#	}
+	managed_state_var :input, 
+		:type => :option, 
+	  :display_order => 1, 
+		:options => ("1".."6").to_a,
+	  :response => :channel,
+		:action => proc{|input|
+			"#{input}!"
+		}
   managed_state_var :video,
     :type => :option,
     :display_order => 1,
@@ -68,20 +68,20 @@ class ExtronVideoSwitcher < VideoSwitcher
       self.audio = m[2].to_i if m[2].to_i > 0
 			self.clipping = (m[3] == "1")
 		}
-    #match :input, /Mod(\d+) (\d)G(\d) (\d)G(\d) (\d)G(\d) (\d)G(\d)=(\d)G(\d)/, proc{|m|
-    #  x1, x2 = [m[10].to_i, m[11].to_i]
-    #  if x1 < 3
-    #    i = (x1-1)*2 + (x2-1) % 2 + 1
-    #  else
-    #    i = (x1-3)*3 + (x2-1) % 3 + 5
-    #  end
+    match :input, /Mod(\d+) (\d)G(\d) (\d)G(\d) (\d)G(\d) (\d)G(\d)=(\d)G(\d)/, proc{|m|
+      x1, x2 = [m[10].to_i, m[11].to_i]
+      if x1 < 3
+        i = (x1-1)*2 + (x2-1) % 2 + 1
+      else
+        i = (x1-3)*3 + (x2-1) % 3 + 5
+      end
       #DaemonKit.logger.debug("INPUT = (#{i}, #{x1}, #{x2})")
-    #  self.input = i
-    #}
+      self.input = i
+    }
 	end
 	
 	requests do
-		#send :input, "I", 0.5
+		send :input, "I", 0.5
     send :video, "I", 0.5
     send :audio, "I", 0.5
 		send :volume, "V", 0.5
