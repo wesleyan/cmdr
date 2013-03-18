722cb0dab64cd9183e26c2d524ac7f110be45581
Brian Gapinski <bgapinski@wesleyan.edu>
2013-03-18 16:22:49 -0400

    Eiki driver mostly functional. Issuses with video mute.

--- lib/roomtrol/devices/EikiProjector.rb
+++ lib/roomtrol/devices/EikiProjector.rb
@@ -9,17 +9,15 @@
 #---
 
 class EikiProjector < Projector  
-  INPUT_HASH = {"RGB1" => 1, "RGB2" => 2, "VIDEO" => 6, "SVIDEO" => 11}
+  INPUT_HASH = {"RGB1" => 5, "RGB2" => 6, "VIDEO" => 7}
 
   configure do
-    DaemonKit.logger.info "@Initializing projector on port #{options[:port]} with name #{name}"
+    DaemonKit.logger.info "@Initializing projector on port #{options[:uri]} with name #{name}"
     port :type => :port
     baud :type => :integer, :default => 19200
     data_bits 8
     stop_bits 1
     parity 0
-    message_end "\r"
-    message_timeout 2.0
   end
 
   managed_state_var :power, 
@@ -32,10 +30,10 @@ class EikiProjector < Projector
 	managed_state_var :input, 
 		:type => :option,
 		# Numbers correspond to HDMI, YPBR, RGB, RGB2, VID, and SVID in that order
-		:options => ['RGB1', 'RGB2', 'VIDEO', 'SVIDEO'] 
+		:options => ['RGB1', 'RGB2', 'VIDEO', 'SVIDEO'] ,
 		:display_order => 2,
 		:action => proc{|source|
-			"C#{INPUT_HASH[source]}\r"
+			"C0#{INPUT_HASH[source]}\r"
 		}
 
 	managed_state_var :video_mute,
@@ -46,23 +44,37 @@ class EikiProjector < Projector
 		}
 		
 	responses do
-		ack ""
-		error :general_error, "?\r", "Received an error"
-		match :power,  /(\d\d)/, proc{|m|
-	  		DaemonKit.logger.info "Received power value #{m[1]}"
-			self.power = (m[1] == "00")
+		ack "\x06"
+		error :general_error, "", "Received an error"
+		match :power,  /((0|2|4|8)0)/, proc{|m|
+			DaemonKit.logger.info "Received power value: #{m[1]}"
+			if m[1] == "00"
+				self.power = true
+			elsif m[1] == "80"
+				self.power = false
+			end
 			self.cooling = (m[1] == "20")
 			self.warming = (m[1] == "40")	
 		}
-		match :video_mute, /0(0|1)/, proc{|m| self.video_mute = (m[1] == "1")}
-		match :input,      /(\d)/, proc{|m| self.input = m[1]}
+		#match :video_mute, /(8(0|1))/, proc{|m| self.video_mute = (m[1] == "81")}
+		match :input, /([1-3])/, proc{|m| 
+			#DaemonKit.logger.debug "Recieved source value: #{m[1]}"
+			if m[1] == "1"
+				self.input = "RGB1"
+			elsif m[1] == "2"
+				self.input = "RGB2"
+			else
+				self.input = "VIDEO"
+			end 
+		}
 	end
 	
 	requests do
            send :power, "CR0\r", 1
            send :source, "CR1\r", 1
-           send :mute, "CRA\r", 1
-           send :lamp_usage, "CR3\r", 0.1
+           #send :mute, "CRA\r", 1
+	   send :clear, "\n", 1
+           #send :lamp_usage, "CR3\r", 0.1
 	end
 
   
