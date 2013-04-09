15737679adb9c7118771708e97a68094c7eb955f
Brian Gapinski <bgapinski@wesleyan.edu>
2013-04-09 12:55:32 -0400

    Th AT-PA100 driver has been updated and should be fully functional.

--- lib/roomtrol/devices/AT-PA100.rb
+++ lib/roomtrol/devices/AT-PA100.rb
@@ -19,20 +19,7 @@ class ATPA100 < Wescontrol::RS232Device
   :type => :boolean,
   :display_order => 3,
   :action => proc{|on|
-    # the mute implementation on the device is kind of useless for
-    # our purposes, because it loses mute when volume changes and
-    # because there is no way to query mute status. so we're going
-    # to emulate mute by setting volume to 0.
-    if on
-      @fake_mute = true
-      this.mute = true
-      @old_vol = this.volume
-      "700%"
-    else
-      @fake_mute = false
-      this.mute = false
-      "7#{"%02" % (@old_vol * 60)}%"
-    end
+    "OA#{on ? 0 : 1}."
   }
 
   def volume=
@@ -50,6 +37,8 @@ class ATPA100 < Wescontrol::RS232Device
   :type => :percentage,
   :display_order => 1,
   :action => proc{|vol|
+    # handles mute turning off on volume change
+    self.mute = false if self.mute
     "7#{"%02d" % (vol * 60)}%"
   }
 
@@ -62,14 +51,15 @@ class ATPA100 < Wescontrol::RS232Device
   }
 
   state_var :mode, :type => :string, :editable => false
+  state_var :operational, :type => :boolean
 
   responses do
     match :mode, /([STEREO|MONO]) MODE/, proc{|m| self.mode = m[1]}
     match :input, /A: (\d) -> 1/, proc{|m| self.input = m[1]}
     match :mic_volume, /Volume of MIC : (\d\d)/, proc{|m| self.mic_volume = m[1].to_i/60.0}
     match :volume, /Volume of LINE : (\d\d)/, proc{|m| self.volume = m[1].to_i/60.0}
-    match :mute, "Mute Audio", proc{ self.mute = true }
-    match :unmute, "UnMute Audio", proc{ self.mute = false }
+    match :unmute, /UnMute Audio/, proc{ self.mute = false }
+    match :mute, /Mute Audio/, proc{ self.mute = true }
   end
 
   requests do
