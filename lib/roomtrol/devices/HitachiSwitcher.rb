1371712ebfde6f8397bf0b55d72b495f076dfce0
Sam Giagtzoglou <sgiagtzoglou@wesleyan.edu>
2013-05-23 15:56:56 -0400

    HitachiSwitcher.rb

---
+++ lib/roomtrol/devices/HitachiSwitcher.rb
@@ -0,0 +1,11 @@
+#---
+#{
+#	"name": "ExtronVideoSwitcher",
+#	"depends_on": "VideoSwitcher",
+#	"description": "Controls Extron video switchers that support SIS",
+#	"author": "Micah Wylde",
+#	"email": "mwylde@wesleyan.edu"
+#}
+#---
+
+class HitachiSwitcher < SocketVideoSwitcher
