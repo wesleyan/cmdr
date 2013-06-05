b5e3f44aa3ceb9633cf872816e10aa91007d3a5a
sgiagtzoglou <sgiagtzoglou@ims-dev.class.wesleyan.edu>
2013-06-05 14:04:43 -0400

    Hitachi Switcher driver done

--- lib/roomtrol/devices/HitachiSwitcher.rb
+++ lib/roomtrol/devices/HitachiSwitcher.rb
@@ -7,74 +7,141 @@
 #	"email": "sgiagtzoglou@wesleyan.edu"
 #}
 #---
+#
+#The switcher uses hex commands, and it has issues with concurrency: it can't handle more than one open socket at a time, 
+#and it loves to stop talking and disconnect for almost no reason. So, instead of using managed_state_vars, state_vars
+#are used to allow the driver code to handle all of the talking to the switcher. Sleep commands are used for rate limiting; 
+#if the switcher is pounded with too many requests it will give up quickly
 
-class HitachiSwitcher < SocketVideoSwitcher
-	configure do 
-		DaemonKit.logger.info "@Initializing HitachiSwitcher at URI #{options[:uri]} with name #{name}"
-		48.times { 
-			send_string("\xBE\xEF\x03\x06\x00\xFE\xD2\x01\x00\x00\x20\x00\x00") #Sets the unused mic volume to 0 to avoid hardware error where input audio is picked up by the mic
-			ack #Waits for a response from device before sending next command
-		}
-
-	end
-	#Hitachi uses hex commands for set and get commands
-	inputCommand = ["\xBE\xEF\x03\x06\x00\xFE\xD2\x01\x00\x00\x20\x00\x00",
-			"\xBE\xEF\x03\x06\x00\x3E\xD0\x01\x00\x00\x20\x04\x00",
-			"\xBE\xEF\x03\x06\x00\x0E\xD2\x01\x00\x00\x20\x03\x00",
-			"\xBE\xEF\x03\x06\x00\x6E\xD6\x01\x00\x00\x20\x0D\x00",
-			"\xBE\xEF\x03\x06\x00\x9E\xD6\x01\x00\x00\x20\x0E\x00",
-			"\xBE\xEF\x03\x06\x00\x3E\xDF\x01\x00\x00\x20\x10\x00"] #Array of input set commands
-
-	inputGet = {"x20\x00\x00" => 1,
-				"x20\x04\x00" => 2,
-				"x20\x03\x00" => 3,
-				"x20\x0D\x00" => 4,
-				"x20\x0E\x00" => 5,
-				"x20\x10\x00" => 6} #Matches the get input command back to an input number
-	
-	managed_state_var :input,
-    	:type => :option,
-    	:display_order => 1,
-    	:options => ("1".."6").to_a,
-    	:response => :channel,
-    	:action => proc{|input|
-    		#DaemonKit.logger.info "Current input: " + self.input ". Changing input to " + input
-      		send_string inputCommand[input-1]  #Sends corresponding source command
-    	}
-	managed_state_var :volume,
-		:type => :percentage,
-		:display_order => 2,
-		:response => :volume,
-		:action => proc{|volume|
-			#DaemonKit.logger.info "Current volume: " + @volume ". Increasing volume to " + volume
-			@volume.upto(volume) do #Increments volume up to inputed volume
-    			send_string "\xBE\xEF\x03\x06\x00\xAB\xC3\x04\x00\x50\x20\x00\x00" 
-        		ack #Waits for a response from device before sending next command
-        	end
-			#DaemonKit.logger.info "Current volume: " + @volume ". Decreasing volume to " + volume
-			volume.upto(@volume) do #Decrements volume down to inputed volume
-    			send_string "\xBE\xEF\x03\x06\x00\x7A\xC2\x05\x00\x50\x20\x00\x00" 
-        		ack #Waits for a response from device before sending next command
-        	end
-		}
-	managed_state_var :mute,
-		:type => :boolean,
-		:display_order => 3,
-		:response => :mute,
-		:action => proc{|on|
-			#DaemonKit.logger.info "Current mute: " + @mute ". Changing mute to " + on
-			on ? send_string "\xBE\xEF\x03\x06\x00\xD6\xD2\x01\x00\x02\x20\x01\x00" : send_string("\xBE\xEF\x03\x06\x00\x46\xD3\x01\x00\x02\x20\x00\x00") #Sends mute or unmute command
-		}
-	
-	responses do
-		self.input = inputGet[:input]+1
-		self.volume = :volume[1].unpack('C')[0] #Recieves packet and decodes hex to get int for volume 0-48
-		self.mute = (:mute[1].unpack('C')[0] == 1) #Recieves packet and decodes hex to get bool for mute
+require "socket"
+class HitachiSwitcher < Wescontrol::Device
+	configure do
+		DaemonKit.logger.info "@Initializing HitachiSwitcher at URI #{uri} with name #{name}"
 	end
 
-	requests do
-		send :input, "\xBE\xEF\x03\x06\x00\xCD\xD2\x02\x00\x00\x20\x00\x00", 1
-		send :volume,"\xBE\xEF\x03\x06\x00\xCD\xC3\x02\x00\x50\x20\x00\x00", 1
-		send :mute,  "\xBE\xEF\x03\x06\x00\x75\xD3\x02\x00\x02\x20\x00\x00", 1
+	state_var :video, :type => :option, :editable => :true, :options => ("1".."6").to_a, :action => proc{|input|
+	    	if self.video != input #If input is not the input the switcher is already on
+	    		DaemonKit.logger.info("Changing switcher input from #{self.video} to #{input}")
+	    		@inputReq = input
+	    		@inputChanging = true
+	    	end
+	    }
+
+	state_var :volume, :type => :percentage, :editable => :true, :action => proc{|volume|
+			@volreq = volume
+			@volChanging = true if @volreq != self.volume
+		}
+
+	 state_var :mute, :type => :boolean, :editable => true, :action => proc{|on|
+	 		@mutereq = on
+	 		@muteChanging = true if self.mute != @mutereq
+	 	}
+	
+	def initialize(a, b)
+		super(a, b)
+		uri = configuration[:uri]
+		@switcherIP = uri.match(/(\d+.\d+.\d+.\d+)/).to_s #IP address of switcher
+		@inputChanging = false
+		@volChanging = false
+		@muteChanging = false
+		@inputReq = 0
+		@volreq = 0
+		@mutereq = false
+		@inputGet = {"0" => [1, "COMP1"],
+			"4" => [2, "COMP2"],
+			"3" => [3, "HDMI1"],
+			"13" => [4, "HDMI2"],
+			"14" => [5, "HDMI3"],
+			"16" => [6, "HDMI4"]} #Matches the get input command back to an input number and name
+	end
+
+	def run
+		super
+		socket = TCPSocket.new(@switcherIP, 23)
+		micvol = sendTCP(socket, "\xBE\xEF\x03\x06\x00\x75\xF1\x02\x00\xA2\x20\x00\x00")[1].unpack('C')[0]
+		DaemonKit.logger.info "Mic vol is #{micvol}, muting" if micvol != 0
+		micvol.times{
+			socket.puts "\xBE\xEF\x03\x06\x00\xC2\xF0\x05\x00\xA2\x20\x00\x00" #Sets the unused mic volume to 0 to avoid hardware issue where input audio is picked up by the mic. Mic doesn't have a mute
+			socket.read 1 #The switcher sends back one packet - this limits the rate
+			}
+		begin
+			while true
+				readIn = sendTCP(socket,"\xBE\xEF\x03\x06\x00\xCD\xD2\x02\x00\x00\x20\x00\x00").unpack('C*')[1].to_s #Gets the input keys that match @inputGet
+				self.video = @inputGet[readIn][0]
+				inputName = @inputGet[readIn][1]
+				self.volume = sendTCP(socket, "\xBE\xEF\x03\x06\x00\xCD\xC3\x02\x00\x50\x20\x00\x00")[1].unpack('C')[0] #Gets volume as an int
+				self.mute = sendTCP(socket, "\xBE\xEF\x03\x06\x00\x75\xD3\x02\x00\x02\x20\x00\x00")[1].unpack('C')[0] == 1 #Gets mute as an bool
+				#DaemonKit.logger.info("Input = #{self.video}:#{inputName}, Volume = #{@volume}, Mute = #{@mute}, InputChanging? = #{@inputChanging}, volChanging? = #{@volChanging}, muteChanging? = #{@muteChanging}")	
+				socket = changeInput(socket, @inputReq) if @inputChanging
+				socket = changeVol(socket, @volreq, self.volume) if @volChanging
+				if @muteChanging
+					@mutereq ? (socket.puts "\xBE\xEF\x03\x06\x00\xD6\xD2\x01\x00\x02\x20\x01\x00") : (socket.puts "\xBE\xEF\x03\x06\x00\x46\xD3\x01\x00\x02\x20\x00\x00") #Sends mute or unmute command
+					socket.read 1
+					@muteChanging = false
+					DaemonKit.logger.info("Changing mute to #{@mutereq}")
+				end
+				sleep 1
+			end
+		rescue Exception => e
+			DaemonKit.logger.error("Switcher Error: #{e.message}")
+			socket.close
+			sleep 1
+			socket = TCPSocket.new(@switcherIP, 23)
+			retry
+		end
+	end
+
+	def sendTCP(socket, hexSend) #Method for sending info commands to switcher. Not used to change things
+		sleep 0.2
+		socket.puts hexSend
+		sleep 0.2
+		return socket.read(3)
+	end
+
+	def changeInput(socket, input)
+		begin
+			inputCommand = ["\xBE\xEF\x03\x06\x00\xFE\xD2\x01\x00\x00\x20\x00\x00",
+				"\xBE\xEF\x03\x06\x00\x3E\xD0\x01\x00\x00\x20\x04\x00",
+				"\xBE\xEF\x03\x06\x00\x0E\xD2\x01\x00\x00\x20\x03\x00",
+				"\xBE\xEF\x03\x06\x00\x6E\xD6\x01\x00\x00\x20\x0D\x00",
+				"\xBE\xEF\x03\x06\x00\x9E\xD6\x01\x00\x00\x20\x0E\x00",
+				"\xBE\xEF\x03\x06\x00\x3E\xDF\x01\x00\x00\x20\x10\x00"] #Array of input set hex commands
+			socket.puts inputCommand[input-1]
+			socket.read 1
+			readIn = sendTCP(socket,"\xBE\xEF\x03\x06\x00\xCD\xD2\x02\x00\x00\x20\x00\x00").unpack('C*')[1].to_s
+			self.video = @inputGet[readIn][0]
+			if self.video == input
+				@inputChanging = false
+			else
+				raise "INPUT CHANGE #{input} to #{self.video} FAILED"
+			end
+		rescue Exception => e
+		 	DaemonKit.logger.error("Error switching input: #{e.message}")
+		 	socket.close
+		 	socket = TCPSocket.new(@switcherIP, 23)
+		 	retry
+		end
+		return socket
+	end
+	def changeVol(socket, newvol, oldvol)
+		DaemonKit.logger.info("Changing volume from #{oldvol} to #{newvol}")
+		begin
+			oldvol.upto(newvol) do #Increments volume up to inputed volume
+    			socket.puts "\xBE\xEF\x03\x06\x00\xAB\xC3\x04\x00\x50\x20\x00\x00"
+    			socket.read 1
+        	end
+			newvol.upto(oldvol) do #Decrements volume down to inputed volume
+     			socket.puts "\xBE\xEF\x03\x06\x00\x7A\xC2\x05\x00\x50\x20\x00\x00"
+     			socket.read 1
+        	end
+		rescue Exception => e
+			DaemonKit.logger.error("Error changing volume: #{e.message})")
+			socket.close
+			socket = TCPSocket.new(@switcherIP,23)
+			self.volume = sendTCP(socket, "\xBE\xEF\x03\x06\x00\xCD\xC3\x02\x00\x50\x20\x00\x00")[1].unpack('C')[0] #Gets volume as an int
+			oldvol = self.volume
+			retry
+		end
+		return socket
 	end
 end
