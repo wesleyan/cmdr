4eaf496855ae1a1e1c9677404104dc48cc58e0c8
Brian Gapinski <bgapinski@wesleyan.edu>
2014-03-04 16:00:32 -0500

    Added licensing

--- lib/cmdr/devices/ExtronMPS409.rb
+++ lib/cmdr/devices/ExtronMPS409.rb
@@ -1,3 +1,20 @@
+# Copyright (C) 2014 Wesleyan University
+#
+# This file is part of cmdr-devices.
+#
+# cmdr-devices is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# cmdr-devices is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with cmdr-devices. If not, see <http://www.gnu.org/licenses/>.
+
 #---
 #{
 #	"name": "ExtronMPS409",
