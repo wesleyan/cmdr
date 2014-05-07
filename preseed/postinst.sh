1943e5e817022d485eab992bd1d70597df46c3a3
Robert Christensen <rchristensen@wesleyan.edu>
2014-05-07 16:15:52 -0400

    Updated postinst to include newer chef release

--- preseed/postinst.sh
+++ preseed/postinst.sh
@@ -1,26 +1,23 @@
 #!/bin/bash
 
-echo $(date)' Downloading the chef deb' >> /var/log/postinst.log
-/usr/bin/wget http://ims-chef.wesleyan.edu/cmdr/chef_11.4.2-1.ubuntu.11.04_amd64.deb
-
-echo $(date)' Installing chef' >> /var/log/postinst.log
-dpkg -i chef_11.4.2-1.ubuntu.11.04_amd64.deb
+echo $(date)' Downloading and installing chef' >> /var/log/postinst.log
+curl -L https://www.opscode.com/chef/install.sh | sudo bash -s -- -v 11.12.4
 
 echo $(date)' Creating chef dir' >> /var/log/postinst.log
 mkdir -p /etc/chef
 
-echo $(date) 'copying the validation and roles files'
-wget http://ims-chef.wesleyan.edu/cmdr/validation.pem
+echo $(date) 'Copying the validation and roles files'
+wget http://sw.wesleyan.edu/cmdr/validation.pem
 mv validation.pem /etc/chef/validation.pem
-wget http://ims-chef.wesleyan.edu/cmdr/roles.json
+wget http://sw.wesleyan.edu/cmdr/roles.json
 mv roles.json /etc/chef/roles.json
-wget http://ims-chef.wesleyan.edu/cmdr/client.rb
+wget http://sw.wesleyan.edu/cmdr/client.rb
 mv client.rb /etc/chef/client.rb
 
-echo $(date) 'removing old client' >> /var/log/postinst.log
-nc -w 1 ims-chef.wesleyan.edu 62085
+echo $(date) 'Removing currently registered client if it exists' >> /var/log/postinst.log
+nc -w 1 sw.wesleyan.edu 62085
 
-echo $(date) 'installing chef' >> /var/log/postinst.log
+echo $(date) 'Configuring chef' >> /var/log/postinst.log
 chef-client -j /etc/chef/roles.json -c /etc/chef/client.rb
 
 echo $(date) 'Installation finished'
